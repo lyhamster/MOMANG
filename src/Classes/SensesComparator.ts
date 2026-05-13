8db863805665bbba547a8d0f04e2b736732e1ea6
lyly dev <lyanne.n619@gmail.com>
2026-05-13 10:36:06 +0200

    feat: add sensesComparator file

---
+++ src/Classes/SensesComparator.ts
@@ -0,0 +1,13 @@
+export default class SensesComparator{
+    sensesA: [];
+    sensesB: [];
+    constructor(sensesA: [],  sensesB: []) {
+        this.sensesA = sensesA;
+        this.sensesB = sensesB;
+    }
+
+    makeComparison () {
+        return this.sensesA.some(sense => this.sensesB.includes(sense));
+    }
+
+}
